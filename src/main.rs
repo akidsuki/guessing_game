5f779d0aaacaf9e350144502e5cce402c3023cd4
Akidsuki <yu-e@variabless.net>
2018-10-01 13:01:22 +0900

    feat: 数字の入力を実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// 標準ライブラリのio。ユーザからの入力を受け付ける
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // ミュータブルなString変数
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
