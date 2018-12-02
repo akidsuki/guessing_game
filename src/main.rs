a396bdbc52f3f9399b5dc25b8d399376b5e3f324
RustyRobot <rustfmtbot@yandex.ru>
2018-12-03 00:39:10 +0300

    rustyrobot formatting

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 extern crate rand;
 
 // 標準ライブラリのio。ユーザからの入力を受け付ける
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -17,7 +17,8 @@ fn main() {
         // ミュータブルなString変数
         let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
@@ -28,12 +29,12 @@ fn main() {
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less    => println!("Too small!"),
+            Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal   => {
+            Ordering::Equal => {
                 println!("You win!");
                 break;
-            },
+            }
         }
     }
 }
